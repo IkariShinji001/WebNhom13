c2309d856595a32bdeca21c058ffff4d60f0165a
Nhi13012003 <mjnhnhi333@gmail.com>
2022-12-01 10:30:55 +0700

    Them data

--- assets/js/ListItem.js
+++ assets/js/ListItem.js
@@ -26,12 +26,26 @@ var Phone = [
     type: "Samsung",
     name: "Samsung Galaxy S22 5G 128GB",
     cost_before: "21.990.000",
-    cost_after: "15.990.000",
+    cost_after: "18.990.000",
     cpu: "Snapdragon 8 Gen 1",
     screen: "6.1 inch",
     ram: "8 GB",
     memory: "128 GB",
     image: "assets/img/namePhone/Samsung/samsung-galaxy-s22.webp",
+    os: "Android 12",
+    pinCapacity: "3700 mAh",
+    image:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22.webp",
+    image2:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22-hong-2.webp",
+    image3:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22-hong-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22-hong-4.webp",
+    madeIn: "Việt Nam",
+    About:
+      "Samsung Galaxy S22 là bước nhảy vọt trong công nghệ video trên thế hệ di động. Đồng thời, điện thoại cũng mở ra loạt cải tiến đột phá hàng đầu hiện nay từ màn hình vát phẳng “nịnh” mắt đến bộ xử lý 4nm tiên tiến đầu tiên trên thế hệ smartphone Samsung.",
+    
   },
   {
     type: "Samsung",
@@ -43,6 +57,20 @@ var Phone = [
     ram: "8 GB",
     memory: "128 GB",
     image: "assets/img/namePhone/Samsung/samsung-galaxy-s22-ultra.webp",
+    os: "Android 12",
+    pinCapacity: "5000 mAh",
+    image:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22-ultra.webp",
+    image2:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22-ultra-do-1.webp",
+    image3:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22-ultra-do-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22-ultra-do-4.webp",
+    madeIn: "Việt Nam",
+    About:
+      "Samsung Galaxy S22 Ultra 5G là siêu phẩm kế thừa tinh hoa Galaxy Note cùng đột phá Galaxy S, tạo nên sức mạnh vô song đỉnh cao. Đây là siêu phẩm tuyệt vời nhất mà Samsung từng mang đến.",
+    
   },
   {
     type: "Samsung",
@@ -54,6 +82,19 @@ var Phone = [
     ram: "8 GB",
     memory: "128 GB",
     image: "assets/img/namePhone/Samsung/samsung-galaxy-s22-bora-purple.webp",
+    os: "Android 12",
+    pinCapacity: "3700 mAh",
+    image:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22-bora-purple.webp",
+    image2:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22-bora-purple-1.webp",
+    image3:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22-bora-purple-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Samsung/samsung-galaxy-s22-bora-purple-4.webp",
+    madeIn: "Việt Nam/Trung Quốc",
+    About:
+      "Bật nét kiêu kỳ, sẵn sàng trendy với phiên bản Samsung Galaxy S22 màu tím (Bora Purple), bạn sẽ trở nên thật thời thượng, ấn tượng và cá tính. Đồng hành đó là những công nghệ tốt nhất của Samsung từ hiệu năng Snapdragon 8 Gen 1, màn hình cao cấp cho đến hệ thống camera chuyên nghiệp.",
   },
   {
     type: "Oppo",
@@ -65,6 +106,19 @@ var Phone = [
     ram: "12 GB",
     memory: "256 GB",
     image: "assets/img/namePhone/Oppo/oppo-find-x5-pro-dd.webp",
+    os: "Android 12 (ColorOs 12)",
+    pinCapacity: "5000 mAh",
+    image:
+      "assets/img/namePhone/Oppo/oppo-find-x5-pro-dd.webp",
+    image2:
+      "assets/img/namePhone/Oppo/oppo-find-x5-pro-trang-2.webp",
+    image3:
+      "assets/img/namePhone/Oppo/oppo-find-x5-pro-trang-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Oppo/oppo-find-x5-pro-trang-4.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Hội tụ những tinh hoa công nghệ - OPPO Find X5 Pro đưa bạn đến trải nghiệm vượt trội từ hiệu năng Snapdragon 8 Gen 1, sạc nhanh 80W, cảm biến Sony cao cấp đến thiết kế độc đáo, điện thoại phô diễn trọn bộ đột phá, khẳng định sức mạnh của chiếc flagship đầu bảng.",
   },
   {
     type: "Oppo",
@@ -76,6 +130,19 @@ var Phone = [
     ram: "3 GB",
     memory: "32 GB",
     image: "assets/img/namePhone/Oppo/oppo-a16k-dd.webp",
+    os: "Android 11",
+    pinCapacity: "4230 mAh",
+    image:
+      "assets/img/namePhone/Oppo/oppo-a16k-dd.webp",
+    image2:
+      "assets/img/namePhone/Oppo/oppo-a16k-black-1.webp",
+    image3:
+      "assets/img/namePhone/Oppo/oppo-a16k-black-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Oppo/oppo-a16k-black-4.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Không chỉ ghi điểm nhờ thiết kế 3D trẻ trung nịnh mắt và thân máy nhỏ nhắn vừa tay cầm, chiếc điện thoại OPPO A16k còn mang lại nhiều trải nghiệm thú vị hơn với công nghệ màn hình bảo vệ mắt, dung lượng pin 4.230mAh cho trải nghiệm dài lâu cùng hàng loạt tính năng vượt trội tầm giá.",
   },
   {
     type: "Oppo",
@@ -87,6 +154,19 @@ var Phone = [
     ram: "12 GB",
     memory: "256 GB",
     image: "assets/img/namePhone/Oppo/oppo-reno8-pro-dd.webp",
+    os: "Android 12 (ColorOs 12.1)",
+    pinCapacity: "4500 mAh",
+    image:
+      "assets/img/namePhone/Oppo/oppo-reno8-pro-dd.webp",
+    image2:
+      "assets/img/namePhone/Oppo/oppo-reno8-pro-xanh-1.webp",
+    image3:
+      "assets/img/namePhone/Oppo/oppo-reno8-pro-xanh-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Oppo/oppo-reno8-pro-xanh-5.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Hội tụ những điểm mạnh của dòng Reno, OPPO Reno8 Pro thể hiện bản lĩnh của phiên bản cao cấp từ hiệu năng Dimensity 8100-Max, bộ xử lý hình ảnh tiên tiến Marisilicon-X, camera đỉnh cao và sạc siêu tốc đến 80W.",
   },
   {
     type: "Oppo",
@@ -98,6 +178,19 @@ var Phone = [
     ram: "8 GB",
     memory: "128 GB",
     image: "assets/img/namePhone/Oppo/oppo-a96-8gb-128gb-dd.webp",
+    os: "Android 11",
+    pinCapacity: "5000 mAh",
+    image:
+      "assets/img/namePhone/Oppo/oppo-a96-8gb-128gb-dd.webp",
+    image2:
+      "assets/img/namePhone/Oppo/oppo-a96-8gb-128gb-hong-1.webp",
+    image3:
+      "assets/img/namePhone/Oppo/oppo-a96-8gb-128gb-hong-2.webp",
+    imageBanner:
+      "assets/img/namePhone/Oppo/oppo-a96-8gb-128gb-hong-4.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Đồng hành cùng xu hướng thiết kế mới, OPPO A96 thể hiện vẻ đẹp hoàn mỹ trong phân khúc, nổi bật, sang chảnh và hiện đại. Chưa dừng lại, smartphone còn là sản phẩm công nghệ tiện lợi với pin lớn 5.000mAh và sạc 33W cho mọi trải nghiệm liền mạch.",
   },
   {
     type: "Iphone",
@@ -108,6 +201,19 @@ var Phone = [
     screen: "6.7 inch",
     memory: "128 GB",
     image: "assets/img/namePhone/Iphone/iphone-14-pro-max.webp",
+    os: "iOS 16",
+    pinCapacity: "29 Giờ",
+    image:
+      "assets/img/namePhone/Iphone/iphone-14-pro-max.webp",
+    image2:
+      "assets/img/namePhone/Iphone/ip-14-pro-max-den-2.webp",
+    image3:
+      "assets/img/namePhone/Iphone/ip-14-pro-max-den-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Iphone/iphone-14-pro-max.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Rất khó để tìm ra chiếc điện thoại nào sang trọng như iPhone 14 Pro Max trên thị trường. Thừa hưởng thiết kế vát phẳng từ thế hệ trước, thủ lĩnh iPhone 14 series khoác lên bộ khung vỏ thép, đầm tay và chắc chắn. ",
   },
   {
     type: "Iphone",
@@ -117,8 +223,21 @@ var Phone = [
     cpu: "Apple A15 Bionic",
     screen: "6.7 inch",
     ram: "6 GB",
-    memory: "128 GB",
+    memory: "256 GB",
     image: "assets/img/namePhone/Iphone/iphone-13-pro-dd-bh.webp",
+    os: "iOS 15",
+    pinCapacity: "4352 mAh",
+    image:
+      "assets/img/namePhone/Iphone/iphone-13-pro-dd-bh.webp",
+    image2:
+      "assets/img/namePhone/Iphone/iphone-13-pro-max-vang-2.webp",
+    image3:
+      "assets/img/namePhone/Iphone/iphone-13-pro-max-vang-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Iphone/iphone-13-pro-max-vang-5.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "iPhone 13 Pro Max xứng đáng là một chiếc iPhone lớn nhất, mạnh mẽ nhất và có thời lượng pin dài nhất từ trước đến nay sẽ cho bạn trải nghiệm tuyệt vời, từ những tác vụ bình thường cho đến các ứng dụng chuyên nghiệp.",
   },
   {
     type: "Vivo",
@@ -131,6 +250,19 @@ var Phone = [
     memory: "32 GB",
     image:
       "assets/img/namePhone/Vivo/637707822857432352_00779950-vivo-y15s-trang-xanh-dd.webp",
+    os: "Android 11",
+    pinCapacity: "5000 mAh",
+    image:
+      "assets/img/namePhone/Vivo/637707822857432352_00779950-vivo-y15s-trang-xanh-dd.webp",
+    image2:
+      "assets/img/namePhone/Vivo/vivo-y15s-trang-xanh-2.webp",
+    image3:
+      "assets/img/namePhone/Vivo/vivo-y15s-trang-xanh-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Vivo/vivo-y15s-trang-xanh-5.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Tha hồ giải trí suốt cả ngày với dung lượng pin lớn 5000mAh, vivo Y15s sẽ cùng bạn tận hưởng những niềm vui với thiết kế mỏng nhẹ, trải nghiệm mượt mà trên màn hình tràn Halo 6,51 inch cực đã.",
   },
   {
     type: "Vivo",
@@ -142,6 +274,19 @@ var Phone = [
     ram: "4 GB",
     memory: "64 GB",
     image: "assets/img/namePhone/Vivo/vivo-t1x-xanh-dd.webp",
+    os: "Android 11",
+    pinCapacity: "5000 mAh",
+    image:
+      "assets/img/namePhone/Vivo/vivo-t1x-xanh-dd.webp",
+    image2:
+      "assets/img/namePhone/Vivo/vivo-t1x-xanh-2.webp",
+    image3:
+      "assets/img/namePhone/Vivo/vivo-t1x-xanh-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Vivo/vivo-t1x-xanh-4.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "vivo T1x mang đến khả năng chơi game ấn tượng trên tay bạn, biến bạn trở thành một game thủ chuyên nghiệp với những trận game cực cháy.",
   },
   {
     type: "Vivo",
@@ -153,6 +298,19 @@ var Phone = [
     ram: "8 GB",
     memory: "128 GB",
     image: "assets/img/namePhone/Vivo/638016881305619480_vivo-y35-den-dd.webp",
+    os: "Android 12",
+    pinCapacity: "5000 mAh",
+    image:
+      "assets/img/namePhone/Vivo/638016881305619480_vivo-y35-den-dd.webp",
+    image2:
+      "assets/img/namePhone/Vivo/vivo-y35-vang-1.webp",
+    image3:
+      "assets/img/namePhone/Vivo/vivo-y35-vang-2.webp",
+    imageBanner:
+      "assets/img/namePhone/Vivo/vivo-y35-vang-4.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Là sản phẩm tiếp nối sự thành công của Y-series, vivo Y35 tiếp tục mang đến một ngôn ngữ thiết kế đặc trưng với những đường nét ấn tượng. Điện thoại còn cung cấp một hiệu năng mạnh mẽ cùng khả năng đa nhiệm tuyệt vời.",
   },
   {
     type: "Vivo",
@@ -165,6 +323,19 @@ var Phone = [
     memory: "128 GB",
     image:
       "assets/img/namePhone/Vivo/637983398315589960_vivo-y22s-xanh-dd.webp",
+      os: "Android 12",
+    pinCapacity: "5000 mAh",
+    image:
+      "assets/img/namePhone/Vivo/637983398315589960_vivo-y22s-xanh-dd.webp",
+    image2:
+      "assets/img/namePhone/Vivo/vivo-y22s-xanh-1.webp",
+    image3:
+      "assets/img/namePhone/Vivo/vivo-y22s-xanh-6.webp",
+    imageBanner:
+      "assets/img/namePhone/Vivo/ivo-y22s-xanh-8.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "vivo Y22s là sản phẩm tiếp theo được giới thiệu cho người dùng với nhiều cải tiến mới mẻ, từ thiết kế tạo trào lưu, camera chụp thiếu sáng ấn tượng đến khả năng vận hành trơn tru.",
   },
   {
     type: "Nokia",
@@ -176,6 +347,19 @@ var Phone = [
     ram: "3 GB",
     memory: "32 GB",
     image: "assets/img/namePhone/Nokia/nokia-c30-xanh-dd.webp",
+    os: "Android 11",
+    pinCapacity: "6000 mAh",
+    image:
+      "assets/img/namePhone/Nokia/nokia-c30-xanh-dd.webp",
+    image2:
+      "assets/img/namePhone/Nokia/nokia-c30-xanh-2.webp",
+    image3:
+      "assets/img/namePhone/Nokia/nokia-c30-xanh-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Nokia/nokia-c30-xanh-4.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Thoải mái trải nghiệm trên màn hình cực lớn và thời lượng pin lên tới 3 ngày của Nokia C30. Với thiết kế bền đẹp và thường xuyên được cập nhật phần mềm, Nokia C30 sẽ đồng hành cùng bạn trong nhiều năm tới một cách ổn định.",
   },
   {
     type: "Nokia",
@@ -187,6 +371,19 @@ var Phone = [
     ram: "6 GB",
     memory: "128 GB",
     image: "assets/img/namePhone/Nokia/nokia-g21-dd-docquyen.webp",
+    os: "Android 11",
+    pinCapacity: "5050 mAh",
+    image:
+      "assets/img/namePhone/Nokia/nokia-g21-dd-docquyen.webp",
+    image2:
+      "assets/img/namePhone/Nokia/nokia-g21-tim-3.webp",
+    image3:
+      "assets/img/namePhone/Nokia/nokia-g21-tim-4.webp",
+    imageBanner:
+      "assets/img/namePhone/Nokia/nokia-g21-tim-5.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Nokia G21 đem đến cho bạn trải nghiệm vượt trội với thời lượng pin lên đến 3 ngày, camera 50MP kết hợp cùng công nghệ AI xuất sắc.",
   },
   {
     type: "Nokia",
@@ -198,6 +395,19 @@ var Phone = [
     ram: "3 GB",
     memory: "32 GB",
     image: "assets/img/namePhone/Nokia/nokia-g11-plus-dd.webp",
+    os: "Android 12",
+    pinCapacity: "5000 mAh",
+    image:
+      "assets/img/namePhone/Nokia/nokia-g11-plus-dd.webp",
+    image2:
+      "assets/img/namePhone/Nokia/nokia-g11-plus-xanh-2.webp",
+    image3:
+      "assets/img/namePhone/Nokia/nokia-g11-plus-xanh-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Nokia/nokia-g11-plus-xanh-4.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Nokia G11 Plus là mẫu smartphone lý tưởng trong phân khúc tầm trung hiện nay, với chất lượng xứng danh với tên tuổi thương hiệu.",
   },
   {
     type: "Xiaomi",
@@ -209,6 +419,19 @@ var Phone = [
     ram: "2 GB",
     memory: "32 GB",
     image: "assets/img/namePhone/Xiaomi/xiaomi-redmi-a1-den-dd-bh.webp",
+    os: "Android Go",
+    pinCapacity: "5000 mAh",
+    image:
+      "assets/img/namePhone/Xiaomi/xiaomi-redmi-a1-den-dd-bh.webp",
+    image2:
+      "assets/img/namePhone/Xiaomi/xiaomi-redmi-a1-den-2.webp",
+    image3:
+      "assets/img/namePhone/Xiaomi/xiaomi-redmi-a1-den-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Xiaomi/xiaomi-redmi-a1-den-4.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Redmi A1 tiếp tục là một chiếc điện thoại giá rẻ mang đến trải nghiệm tuyệt vời từ Xiaomi với mặt lưng da sang trọng, màn hình lớn, giao diện Android gốc dễ sử dụng, camera kép và viên pin dung lượng lên tới 5000 mAh.",
   },
   {
     type: "Xiaomi",
@@ -220,6 +443,19 @@ var Phone = [
     ram: "2 GB",
     memory: "32 GB",
     image: "assets/img/namePhone/Xiaomi/xiaomi-redmi-9a.webp",
+    os: "Android 10",
+    pinCapacity: "5000 mAh",
+    image:
+      "assets/img/namePhone/Xiaomi/xiaomi-redmi-9a.webp",
+    image2:
+      "assets/img/namePhone/Xiaomi/xiaomi-redmi-9a-xanhla-2.webp",
+    image3:
+      "assets/img/namePhone/Xiaomi/xiaomi-redmi-9a-xanhla-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Xiaomi/xiaomi-redmi-9a-xanhla-4.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Xiaomi Redmi 9A thể hiện sự vượt trội trong phân khúc giá rẻ khi sở hữu màn hình lớn, thời lượng pin suốt cả ngày và cấu hình tuyệt đỉnh, mang đến cho bạn trải nghiệm thực sự hài lòng.",
   },
   {
     type: "Xiaomi",
@@ -231,6 +467,19 @@ var Phone = [
     ram: "8 GB",
     memory: "128 GB",
     image: "assets/img/namePhone/Xiaomi/xiaomi-12t-den.webp",
+    os: "Android 12 (MIUI 13)",
+    pinCapacity: "5000 mAh",
+    image:
+      "assets/img/namePhone/Xiaomi/xiaomi-12t-den.webp",
+    image2:
+      "assets/img/namePhone/Xiaomi/xiaomi-12t-den-1.webp",
+    image3:
+      "assets/img/namePhone/Xiaomi/xiaomi-12t-den-3.webp",
+    imageBanner:
+      "assets/img/namePhone/Xiaomi/xiaomi-12t-den-4.webp",
+    madeIn: "Trung Quốc",
+    About:
+      "Xiaomi 12T thừa hưởng những điểm nổi bật về thiết kế, hệ thống camera mà thế hệ trước đã làm cũng như được khai phá thêm về sức mạnh hiệu năng.",
   },
 ];
 
