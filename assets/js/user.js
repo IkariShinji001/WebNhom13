ca8f409a4097aa5bd401499db0dfdfa4045c34b6
duy <duytran2003dev@gmail.com>
2022-12-06 08:01:19 +0700

    check user đã đăng nhập chưa, sửa header

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,5 +1,9 @@
 var userBtn = document.querySelector(".user");
 
+if (localStorage.getItem("isLogin") == undefined) {
+  localStorage.setItem("isLogin", 0);
+}
+
 userBtn.addEventListener("click", () => {
   var checkLogin = localStorage.getItem("isLogin");
   console.log(checkLogin);
